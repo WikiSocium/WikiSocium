0bb070286515a687a2feac9f29484d46edbd81ab
Vladimir Kukushkin <kukushechkin@Vladimir-Kukushkins-MacBook.local>
2011-11-20 14:17:22 +0400

    Violation problem for TestUser

---
+++ node/public/javascripts/controllers/Violation.js
@@ -0,0 +1,22 @@
+// [TODO] Сделать наследование от базового класса
+
+var requestedCaseController = 
+{
+    GetNextStepForStep: function(currentStep)
+    {
+        var cStep = parseInt(currentStep);
+        switch(cStep)
+        {
+            case 0:
+                // Как у нас здесь досутпен объект кейса?
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+        }
+    }
+}
