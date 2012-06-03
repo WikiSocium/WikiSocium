40f2945f0bb424031cc3ed9ab953700fe7055878
dlistvin <dlistvin@dlistvin-K53SV.(none)>
2012-06-03 13:42:36 +0400

    reformal widget added

---
+++ node/public/javascripts/reformal.js
@@ -0,0 +1,17 @@
+    var reformalOptions = {
+        project_id: 53131,
+        project_host: "wikisocium.reformal.ru",
+        tab_orientation: "left",
+        tab_indent: "50%",
+        tab_bg_color: "#F05A00",
+        tab_border_color: "#FFFFFF",
+        tab_image_url: "http://tab.reformal.ru/T9GC0LfRi9Cy0Ysg0Lgg0L%252FRgNC10LTQu9C%252B0LbQtdC90LjRjw==/FFFFFF/2a94cfe6511106e7a48d0af3904e3090/left/1/tab.png",
+        tab_border_width: 2
+    };
+    
+    (function() {
+        var script = document.createElement('script');
+        script.type = 'text/javascript'; script.async = true;
+        script.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'media.reformal.ru/widgets/v3/reformal.js';
+        document.getElementsByTagName('head')[0].appendChild(script);
+    })();
