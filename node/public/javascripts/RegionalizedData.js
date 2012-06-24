a1e718edc99ef33d04238a8fe0958064e90fd460
Vladimir Kukushkin <kukushechkin@Vladimirs-MacBook-Pro.local>
2012-06-24 11:39:37 +0400

    improving document widget

--- node/public/javascripts/RegionalizedData.js
+++ node/public/javascripts/RegionalizedData.js
@@ -1,17 +1,46 @@
+var asyncLoop = function(o)
+{
+    var i =- 1;
+    var loop = function()
+    {
+        i++;
+        if(i == o.length)
+        {
+            o.callback();
+            return;
+        }
+        o.functionToLoop(loop, i);
+    } 
+    loop(); //init
+}
+
 RegionalizedData = 
 {
+    downloadedData: [],
+    
     GetDataFromDBWithRegion: function(region, db, dataId, callback)
     {
-        $.ajax({
-            url: '/GetRegionalizedData'
-            , type:'POST'
-            , data: 'region=' + encodeURIComponent(region) + '&db=' + encodeURIComponent(db) + '&dataId=' + encodeURIComponent(dataId)
-            , success: function(res) {
-                    callback(res);
-        		}
-        	, error: function(jqXHR, textStatus, errorThrown) {
-        	        // [TODO]
-                }
-          });
+        RegionalizedData.downloadedData = [];
+        asyncLoop({
+            length: dataId.length,
+            functionToLoop: function(loop, i) {
+                $.ajax({
+                    url: '/GetRegionalizedData'
+                    , type:'POST'
+                    , data: 'region=' + encodeURIComponent(region) + '&db=' + encodeURIComponent(db) + '&dataId=' + encodeURIComponent(dataId[i])
+                    , success: function(res) {
+                            //callback(res);
+                            RegionalizedData.downloadedData.push(res);
+                            loop();
+                		}
+                	, error: function(jqXHR, textStatus, errorThrown) {
+                	        // [TODO]
+                        }
+                  });
+              },
+              callback: function() {
+                  callback(RegionalizedData.downloadedData);
+              }
+        });
     }
 }
