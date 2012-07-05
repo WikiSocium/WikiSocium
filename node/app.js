d8419be62bb9e9ea808d777f9692e9302f8ee152
dlistvin <dlistvin@dlistvin-K53SV.(none)>
2012-07-05 20:17:39 +0400

    debuga

--- node/app.js
+++ node/app.js
@@ -421,22 +421,21 @@ app.get('/auth/vkontakte', loadUser, function(req, res) {
   				if (!error && response.statusCode == 200) {
   					console.log(body);
   					var answer = JSON.parse(body);
-  					res.render('index', {
-          'title':answer,
-          'user':req.currentUser,
-          'menu':res.menu,
-          'headerStats': res.headerStats,
-          'categoryList' : categories,
-          'scripts':[],
-          'styles':[]
-        });
+  					  res.render('about', {
+    'title':"О проекте",
+    'user':req.currentUser,
+    'menu':res.menu,
+    'headerStats': res.headerStats,
+    'scripts':[],
+    'styles':[]
+  }); 
 	   			}	
   			})
   						
    		}	
   	})
 	
-  res.redirect('/');
+  //res.redirect('/');
 });
 //
 // Обработка запроса на показ списка проблем
