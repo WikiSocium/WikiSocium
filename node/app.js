8532be2fa6ae0ce4c48f9a1c10f7de786faeac6e
Denis Sumin <da.sumin@gmail.com>
2012-10-24 13:00:55 +0400

    App.js cleanup: deleted Deimos vk auth code

--- node/app.js
+++ node/app.js
@@ -475,36 +475,6 @@ app.get('/About/:PageName', loadUser, generateMenu, getHeaderStats, function(req
 });
 
 //
-// Это обработка редиректа с вконтакта (тест)
-app.get('/auth/vkontakte', loadUser, function(req, res) {
-	console.log('ololololo');
-	var code = req.query.code;
-	console.log(code);
-	
-  	request({uri:'https://oauth.vk.com/access_token?client_id='+'2981571'+'&client_secret='+'mJloUt73SYT6K9vFmxfi'+'&code='+code}, function (error, response, body) {
-  		if (!error && response.statusCode == 200) {
-  			console.log(body);
-  			var answer = JSON.parse(body);
-  			console.log(answer);
-  			console.log('...');
-  			console.log(answer.user_id); 
-  			
-  			request({uri:'https://api.vk.com/method/getProfiles?uid='+answer.user_id+'&access_token='+answer.access_token}, function (error, response, body) {
-  				if (!error && response.statusCode == 200) {
-  					console.log(body);
-  					var answer = JSON.parse(body);
-  					//У переменной answer есть три поля: uid, first_name и last_name.
-  					console.log(answer);
-  					res.redirect('/');
-	   			}	
-  			})
-  						
-   		}	
-  	})
-	
-  //res.redirect('/');
-});
-//
 // Обработка запроса на показ списка проблем
 app.get('/Problems', loadUser, generateMenu, getHeaderStats, function(req, res){
   
