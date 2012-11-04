6e4c494484a1fd781992185122b5d5ee18312bf4
kukushechkin <kukushechkin@mac.com>
2012-11-04 15:08:12 +0400

    обратное геокодирование для YandexMapsWidgets

--- node/public/javascripts/customWidgets/YandexMapsWidget.js
+++ node/public/javascripts/customWidgets/YandexMapsWidget.js
@@ -10,18 +10,12 @@ function YandexMapsWidget(_widgetid, value)
     var val = {};
   }
   
-  console.log("creating " + _widgetid + " object with: ");
-  console.log(val);
-  
   $("#YandexMapsWidgetInputWS_" + this.widgetid).val(val.address);
   window[this.widgetid + "_map_coords"] = val.coords;
 };
 
 YandexMapsWidget.prototype.getValue = function()
 {
-    console.log("getValue: ");
-    console.log(window[this.widgetid + "_map_coords"]);
-    
     return {address : $("#YandexMapsWidgetInputWS_" + this.widgetid).val(),
             coords  : window[this.widgetid + "_map_coords"]};
 };
