dc0384bc9b809628d89ee4cab555898b2aa3b1c6
vasay <rfhjkbyf>
2012-11-22 22:25:20 +0400

    Исправление бага в DatePicker

--- node/public/javascripts/customWidgets/DatePickerWidget.js
+++ node/public/javascripts/customWidgets/DatePickerWidget.js
@@ -45,10 +45,30 @@ function DatePickerWidget(element, parentEl, value)
   $(this.element).val(value);
 };
 
+function censor(censor) {
+  return (function() {
+    var i = 0;
+
+    return function(key, value) {
+      if(i !== 0 && typeof(censor) === 'object' && typeof(value) == 'object' && censor == value) 
+        return '[Circular]'; 
+
+      if(i >= 29) // seems to be a harded maximum of 30 serialized objects?
+        return '[Unknown]';
+
+      ++i; // so we know we aren't using the original object anymore
+
+      return value;  
+    }
+  })(censor);
+}
+
 // Получение значения виджета.
 DatePickerWidget.prototype.getValue = function()
 { 
-  // Для совместимости храним дату в формате GMT.
+  if($(this.element).val() == "")
+    return "";
+  
   var dateString = $(this.element).datepicker("getDate").toGMTString();
   
   return dateString;
